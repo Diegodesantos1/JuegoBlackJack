bad13f13684c7e78cd886b771b20341c73455705
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-16 17:14:04 +0100

    Actualizadas las funciones, acorde al número de cartas y arreglo del bucle

--- BlackJack.py
+++ BlackJack.py
@@ -15,16 +15,21 @@ cartas = {
     10:chr(0x1f0ad), 
     10:chr(0x1f0ae), 
 }
+numcartas=52
 cartas=[11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 Cartasquellevas=[]
 Cartasquellevabanca=[]
+numcartas=52
 def tomar_carta():
+    global numcartas
     numerorandom= random.randint (2, 11)
     carta_elegida=cartas.pop(numerorandom)
     print(f"Tienes esta carta: {carta_elegida}")
     Cartasquellevas.append(carta_elegida)
+    numcartas-=1
 
 def tomar_2carta():
+    global numcartas
     numerorandom= random.randint (2, 11)
     carta_elegida=cartas.pop(numerorandom)
     print(f"Tienes esta carta: {carta_elegida}")
@@ -33,16 +38,20 @@ def tomar_2carta():
     carta_elegida=cartas.pop(numerorandom)
     print(f"Tienes esta carta: {carta_elegida}")
     Cartasquellevas.append(carta_elegida)
+    numcartas -= 2
 
 def banca_tomar_carta():
+    global numcartas
     global puntuacion_banca
     puntuacion_banca=0
     numerorandom= random.randint (2, 11)
     puntuacion_banca=puntuacion_banca + numerorandom
     carta_elegida_banca=cartas.pop(numerorandom)
     Cartasquellevabanca.append(carta_elegida_banca)
+    numcartas-= 1
 
 def banca_tomar_2carta():
+    global numcartas
     global puntuacion_banca
     puntuacion_banca=0
     numerorandom= random.randint (2, 11)
@@ -53,9 +62,7 @@ def banca_tomar_2carta():
     puntuacion_banca=puntuacion_banca + numerorandom
     carta_elegida_banca=cartas.pop(numerorandom)
     Cartasquellevabanca.append(carta_elegida_banca)
-
-
-
+    numcartas-=2
 
 
 def pasar_turno():
@@ -73,39 +80,19 @@ def plantarse():
     elif tu_puntuacion > puntuacion_banca and tu_puntuacion < 21:
         print("Ganaste")
 
+def juego():
+    print ("¿Qué deseas hacer?" + " 1: tomar otra carta, 2: pasar turno, 3: plantarse")
+    eleccion=int(input())
+    while eleccion != 3 or cartas :
+        tomar_carta()
+        print(Cartasquellevas)
+    while eleccion == 2:
+        pasar_turno()
+    while eleccion ==3:
+        plantarse()
+
 print("Comienza el blackjack")
-#Primer turno
 tomar_2carta()
 banca_tomar_2carta()
 print(Cartasquellevas)
-print ("¿Qué deseas hacer?" + " 1: tomar otra carta, 2: pasar turno, 3: plantarse")
-eleccion=int(input())
-if eleccion == 1:
-    tomar_carta()
-    print(Cartasquellevas)
-elif eleccion == 2:
-    pasar_turno()
-elif eleccion ==3:
-    plantarse()
-
-#Tercer turno
-print ("¿Qué deseas hacer?" + " 1: tomar otra carta, 2: pasar turno, 3: plantarse")
-eleccion=int(input())
-if eleccion == 1:
-    tomar_carta()
-elif eleccion == 2:
-    pasar_turno()
-elif eleccion ==3:
-    plantarse()
-
-tomar_carta()
-banca_tomar_carta()
-#Cuarto turno
-print ("¿Qué deseas hacer?" + " 1: tomar otra carta, 2: pasar turno, 3: plantarse")
-eleccion=int(input())
-if eleccion == 1:
-    tomar_carta()
-elif eleccion == 2:
-    pasar_turno()
-elif eleccion ==3:
-    plantarse()
+juego()
