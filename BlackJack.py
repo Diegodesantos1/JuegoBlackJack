e7a2cd3a7779f632c0c4c823951a8bd1cb613d9e
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-16 15:11:33 +0100

    Creada la elección de la primera carta

--- BlackJack.py
+++ BlackJack.py
@@ -1,18 +1,21 @@
 import random
 
 cartas = { 
-    chr(0x1f0a1): 11, 
-    chr(0x1f0a2): 2, 
-    chr(0x1f0a3): 3, 
-    chr(0x1f0a4): 4, 
-    chr(0x1f0a5): 5, 
-    chr(0x1f0a6): 6, 
-    chr(0x1f0a7): 7, 
-    chr(0x1f0a8): 8, 
-    chr(0x1f0a9): 9, 
-    chr(0x1f0aa): 10, 
-    chr(0x1f0ab): 10, 
-    chr(0x1f0ad): 10, 
-    chr(0x1f0ae): 10, 
+    11:chr(0x1f0a1), 
+    2:chr(0x1f0a2), 
+    3:chr(0x1f0a3), 
+    4:chr(0x1f0a4), 
+    5:chr(0x1f0a5), 
+    6:chr(0x1f0a6), 
+    7:chr(0x1f0a7), 
+    8:chr(0x1f0a8), 
+    9:chr(0x1f0a9), 
+    10:chr(0x1f0aa), 
+    10:chr(0x1f0ab), 
+    10:chr(0x1f0ad), 
+    10:chr(0x1f0ae), 
 }
-print(cartas)
+
+numerorandom= random.randint (2, 11)
+cartas_elegidas=cartas.pop(numerorandom)
+print(f"Tienes esta carta: {cartas_elegidas}")
